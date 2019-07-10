5dfa0744297ed8b9957764cc7d1d268e0289d1cc
Yi Wu <y456wu@uwaterloo.ca>
2019-07-10 13:59:24 -0400

    initialization

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+               hello
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
